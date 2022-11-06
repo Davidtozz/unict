852f38327051cbd4ef2db6a133e3a734c0598383
Davide Pulvirenti <davidepulvirenti2013@gmail.com>
2022-11-06 20:42:25 +0100

    pointers

--- pointers.c
+++ pointers.c
@@ -3,31 +3,17 @@
 
 int main(void) {
 
-    // long int x = 5;
-    // long int *pX = &x;
-
-    // printf("x = %d\n", x);
-    // printf("pX = %d\n", *pX);
-
-    // printf("\nsizeof x: %ld", sizeof(x));
-    // printf("\nsizeof pX: %ld\n", sizeof(pX));
-
     int a = 5;
 
     int* b = &a;
     int* c = &a;
 
     printf("a = %d\n", a);
-
-
-
     printf("b = %d\n", *b);
     
     *b = 8;
 
     printf("c = %d\n", *c);
     printf("a = %d", a);
-
-    
 
 }
